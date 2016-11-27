f07656cc5ef8ff10d057d3f900742be2141741fb
CAIJIN <cba46532@gmail.com>
2016-11-27 22:34:15 +0800

    123

---
+++ WENG/vendor/laravel/framework/src/Illuminate/Auth/Notifications/ResetPassword.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Illuminate\Auth\Notifications;
+
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPassword extends Notification
+{
+    /**
+     * The password reset token.
+     *
+     * @var string
+     */
+    public $token;
+
+    /**
+     * Create a notification instance.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array|string
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Build the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('您收到此電子郵件是因為我們收到了您帳戶的密碼重設要求。')
+            ->action('重設密碼', url('password/reset', $this->token))
+            ->line('如果您未請求重置密碼，則無需進一步操作。');
+    }
+}
