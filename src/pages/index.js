4f22c0526ef8a7bb355f645ae03e755d99f39388
翁才晉 <cba46532@gmail.com>
2020-02-01 01:32:24 +0800

    feat: change project preview image.

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,9 +22,10 @@ export default function HomePage() {
         async
         {` {`}
         <h3>
-          A Passionate Front-end Developer, UI/UX Enthusiast, Android Geek and Web Developer. Lorem
-          ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et.
+          高三接觸網頁程式設計，熱愛學習新技術與分享，享受在技術變化萬千的世界當中，對不同領域帶有學習熱枕，樂於學習接收新鮮事物。
+          <br />
+          我有 2 年以上的網頁前端開發經驗，參與過 6 個 以上的專案開發，收悉 React 、 Vue
+          ，具備獨立思考與解決問題的能力，同時也熟悉與開發團隊合作開發。
         </h3>
         {`}`}
       </StyledSubTitle>
--- src/pages/projectConfig.js
+++ src/pages/projectConfig.js
@@ -2,9 +2,10 @@ import JeanCarePOS from 'assets/previews/JeanCarePOS.png'
 import LineCoupon from 'assets/previews/LineCoupon.png'
 import MLGCtrl from 'assets/previews/MLGCtrl.jpg'
 import MLGOffcialWebsite from 'assets/previews/MLGOffcialWebsite.png'
-import MLGVIP from 'assets/previews/MLGVIP.png'
+// import MLGVIP from 'assets/previews/MLGVIP.png'
 import Melegant from 'assets/previews/Melegant.png'
-import BeautyOS from 'assets/previews/BeautyOS.png'
+import MLGVIPLogin from 'assets/previews/MLGVIPLogin.png'
+// import BeautyOS from 'assets/previews/BeautyOS.png'
 
 export default [
   {
@@ -12,46 +13,48 @@ export default [
     title: '名留集團官網',
     link: 'https://www.mlgroup.io/',
     discription:
-      '名留集團官網提供集團的介紹及相關和美有關的文章， 除此之外也有集團旗下品牌店家的查詢與理念介紹。上線至今已累計 340,000+ 的使用者透過網站來更瞭解名留集團。'
+      '名留集團官網提供集團的介紹及相關和美有關的文章， 除此之外也有集團旗下品牌店家的查詢與理念介紹。上線至今已累計 410,000+ 的使用者透過網站來更瞭解名留集團。'
   },
   {
     image: MLGCtrl,
     title: '官網管理系統',
     link: '',
     discription:
-      '官網管理系統負責處理相關集團官網有關的資料設定，供行銷人員更有效的管理官網，如: 像臉書的粉絲團文章發佈功能等等...'
+      '官網管理系統負責處理相關集團官網有關的資料設定，供行銷人員更有效的管理官網，減少了 70% 行銷人員管理官網所花費的時間。'
   },
   {
-    image: MLGVIP,
+    image: MLGVIPLogin,
     title: '名留內部系統',
     link: '',
     discription:
-      '內部系統供所有旗下 4 大品牌 2500 名設計師使用，目的在於減少內部溝通成本。目前已夠過內部系統成功配合舉辦 5 場大型活動。'
+      '內部內部系統供所有旗下 4 大品牌 2100+ 名設計師使用，目的在於減少內部溝通成本, 減少 80% 與店家溝通安排時程時間。 目前已夠過內部系統成功配合舉辦 5 場大型活動。'
   },
   {
     image: Melegant,
     title: '美樂跟',
     link: 'https://www.melegant.com/',
     discription:
-      '美樂跟公司是名留集團旗下的一個新創品牌， 提供消費者和美相關的解決方案，至今已有 5000+ 的使用者。'
+      '美樂跟提供消費者和美相關的解決方案，目前已經有 50000+ 的使用者透過美樂跟尋找適合自己的髮型或是設計師。'
   },
   {
     image: JeanCarePOS,
     title: 'JeanCare POS',
     link: '',
-    discription: '消費者可以透過 LINE(LIFF) 預約服務，店家則可以使用 POS 進行結帳及客戶管理等功能。'
+    discription:
+      '店家可以使用 JeanCare POS 進行結帳及客戶管理等功能，消費者可以使用 LINE 預約服務，現已有 800+ 消費者透過 JeanCare POS 預約服務。'
   },
   {
     image: LineCoupon,
-    title: '店家行銷系統(LIFF)',
-    link: '',
-    discription: '設計師可以透過LINE販售美髮相關優惠, 消費者可以將已購買的商品轉贈給他人使用。'
-  },
-  {
-    image: BeautyOS,
-    title: 'BeautyOS',
+    title: '店家行銷相關系統',
     link: '',
     discription:
-      'BeautyOS 是一個給美髮產業專用的 POS 系統, 提供美髮產業不同於一般 POS 系統的功能, 整合顧客管理、行銷、業績...等功能。目前有 200 間美髮店面使用它。'
+      '此系統目的將以往線下的行銷活動帶到線上，並配合每年公司大型活動，現已線上化 90%, 同期業績提升 25% 。'
   }
+  // {
+  //   image: BeautyOS,
+  //   title: 'BeautyOS',
+  //   link: '',
+  //   discription:
+  //     'BeautyOS 是一個給美髮產業專用的 POS 系統, 提供美髮產業不同於一般 POS 系統的功能, 整合顧客管理、行銷、業績...等功能。目前有 200 間美髮店面使用它。'
+  // }
 ]
